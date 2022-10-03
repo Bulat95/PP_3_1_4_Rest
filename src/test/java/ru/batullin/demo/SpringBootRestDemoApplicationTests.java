08fc76eb9ee4365b80efff37faa7656d3a8a50d0
Bulat95 <49592201+Bulat95@users.noreply.github.com>
2022-10-03 22:03:01 +0400

    fixed 2.0

---
+++ src/test/java/ru/batullin/demo/SpringBootRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.batullin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
